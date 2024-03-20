2c0f409e81f507f64d3679f4cb37843f6e5f0b5f
Erick Wendel <erick.workspace@gmail.com>
2024-03-20 15:30:39 -0500

    refactoring
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

---
+++ test/unit/listBuckets.test.js
@@ -0,0 +1,42 @@
+import {
+    describe,
+    it,
+    expect,
+    jest
+} from '@jest/globals'
+
+import { S3 } from './../../src/factory.js'
+import { main } from './../../src/index.js'
+
+describe('Unit Tests', () => {
+
+    it('it should return an array with a S3 Bucket', async () => {
+        const myBuckets = [
+            {
+                Name: "dvds-piratas001"
+            },
+            {
+                Name: "foto-de-familia001"
+            },
+        ]
+
+        const S3Mock = {
+            listBuckets: jest.fn().mockReturnThis(),
+            promise: jest.fn().mockResolvedValue({
+                Buckets: myBuckets
+            })
+        }
+
+        jest.spyOn(S3, 'listBuckets').mockReturnValue(S3Mock)
+
+
+        const response = await main()
+        const { allBuckets: { Buckets } } = JSON.parse(response.body)
+
+        expect(S3.listBuckets).toBeCalledTimes(1)
+        expect(S3.listBuckets().promise).toBeCalledTimes(1)
+
+        expect(Buckets).toStrictEqual(myBuckets)
+        expect(response.statusCode).toStrictEqual(200)
+    })
+})
